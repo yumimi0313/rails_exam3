517657ab073a630a54d96955d8d75e16ef66bab2
yumi yonemori <yumiyonemori@gmail.com>
2023-02-14 13:10:30 +0900

    お気に入り機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,11 @@
 class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.create(post_id: params[:post_id])
+    redirect_to posts_path, notice: "#{favorite.post.user.name}さんのポストをお気に入りに登録しました"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to posts_path, notice: "#{favorite.post.user.name}さんのポストをお気に入りを解除しました"
+  end
 end
